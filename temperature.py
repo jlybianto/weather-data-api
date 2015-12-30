d675deb021dad770aa1a8bcd9243ee63f1bb6bb3
jlybianto <jesse.lybianto@gmail.com>
2015-12-30 14:56:57 -0500

    Add headings and import packages

---
+++ temperature.py
@@ -0,0 +1,23 @@
+# ----------------
+# IMPORT PACKAGES
+# ----------------
+
+# Requests is a package that allows download of data from any online resource.
+# The sqlite3 model is used to work with the SQLite database.
+# The datetime package has datetime objects.
+# The pandas package is used to fetch and store data in a DataFrame.
+# The json_normalize package is to convert data into a pandas DataFrame from a JSON format.
+
+import requests
+import sqlite3 as lite
+import datetime
+import pandas as pd
+from pandas.io.json import json_normalize
+
+# ----------------
+# OBTAIN DATA
+# ----------------
+
+# ----------------
+# STORE DATA
+# ----------------
