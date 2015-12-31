fa1b7307051397932f3735dfe54bbbd7f9f68056
jlybianto <jesse.lybianto@gmail.com>
2015-12-31 02:48:53 -0500

    Add query to create date column

--- temperature.py
+++ temperature.py
@@ -48,4 +48,15 @@ cur = con.cursor()
 
 # Drop currently existing tables.
 with con:
-	cur.execute("DROP TABLE IF EXISTS daily_temp")
+	cur.execute("DROP TABLE IF EXISTS maxTemp")
+
+# Create the table specifying the name of columns and their data types.
+# First column wold be date (DATE type).
+date = []
+date.append(startDate)
+for t in range(30):
+	date.append(date[-1] + datetime.timedelta(days=1))
+date = [(t.strftime("%Y/%m/%d")) for t in date]
+
+# Remaining columns are the five selected cities (NUMERIC type).
+cityId = [i + " NUMERIC" for i in cities.keys()]
